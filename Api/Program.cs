104abdc9220c72e8998ef43774f4aeefca0f1cc3
Ignacio Cabral <ignaciocabral97@gmail.com>
2024-08-31 00:07:01 -0300

    cors

--- Api/Program.cs
+++ Api/Program.cs
@@ -5,6 +5,15 @@ builder.Services.AddControllers();  // Registra los controladores
 builder.Services.AddEndpointsApiExplorer();  // Explora los endpoints
 builder.Services.AddSwaggerGen();  // Configura Swagger para la documentación
 
+// Configurar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("https://ignaciocabral.com.ar") // Reemplaza con tu dominio
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
 var app = builder.Build();
 
 // Configuración del pipeline de solicitudes HTTP.
@@ -17,6 +26,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();  // Redirige HTTP a HTTPS
 
 app.UseRouting();  // Habilita el enrutamiento
+
+// Aplica la política CORS
+app.UseCors("AllowSpecificOrigin");
 
 app.MapControllers();  // Mapea los controladores a las rutas
 
